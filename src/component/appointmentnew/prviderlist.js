0bfc31dfd21b4ae57fd0de9478e4922493cbd72e
utkarshhbtu2021 <utkarshhbtu2021@gmail.com>
2023-01-07 10:35:06 +0530

    done

--- src/component/appointmentnew/prviderlist.js
+++ src/component/appointmentnew/prviderlist.js
@@ -52,7 +52,7 @@ const ProviderList = (props) => {
     if (dd < 10) dd = "0" + dd;
     if (mm < 10) mm = "0" + mm;
     const date = mm + "/" + dd + "/" + yyyy;
-    const data = rawInformation.filter((el) => el.date === date);
+    const data = rawInformation?.filter((el) => el.date === date);
     return data;
   };
 
